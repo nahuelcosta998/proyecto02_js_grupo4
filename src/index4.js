0f94e281c0a4cb49952bf00aafa43ce8fecf7486
ExequielDani <exedaniel.09@gmail.com>
2025-09-01 23:55:11 -0300

    punto 4
    se comento el punto 4

--- src/index4.js
+++ src/index4.js
@@ -1,8 +1,10 @@
+/* function toma los valores de los formularios */
 function mostrarDatos() {
     let nombre = document.getElementById("nombre").value;
     let apellido = document.getElementById("apellido").value;
     let libreta = document.getElementById("libreta").value;
 
+    /* lanza un cartel al hacer click al boton */
     alert(
         "Los datos ingresados son: " +
         "Nombre: " + nombre + " " +
